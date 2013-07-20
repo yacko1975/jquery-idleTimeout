9ba7be26a7f8e993e0707c5f350c90b48f3d51dd
Jose Garcia <jose.balius@gmail.com>
2013-07-20 15:51:20 -0400

    Update jquery-idleTimeout.js

--- jquery-idleTimeout.js
+++ jquery-idleTimeout.js
@@ -4,6 +4,8 @@
 //## visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter 
 //## to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 //######
+// Modified By: Jose Garcia
+// Email: jose.balius@gmail.com
 
 (function($) {
     $.fn.idleTimeout = function(options) {
