1ab82b4e2816e248444ff9e70765db341bafe8b6
Jill Elaine <jillelaine01@gmail.com>
2013-08-22 09:29:14 -0700

    Update code format to standards

--- jquery-idleTimeout-for-testing.js
+++ jquery-idleTimeout-for-testing.js
@@ -1,19 +1,17 @@
-//######
-//## This work is licensed under the Creative Commons Attribution-Share Alike 3.0
-//## United States License. To view a copy of this license,
-//## visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter
-//## to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
-//######
-
-// Dependency: https://github.com/marcuswestin/store.js - version 1.3.4+
-
-// Thanks to https://github.com/philpalmieri/jquery-idleTimeout & https://github.com/josebalius/jquery-idleTimeout
-// Modified By: Jill Elaine
-// Email: jillelaine01(at)gmail(dot)com
-
-// version 1.0.2
-// Comments and Console logs for debugging with Firefox & Firebug
-
+/**
+ * This work is licensed under the Creative Commons Attribution-Share Alike 3.0
+ * United States License. To view a copy of this license,
+ * visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter
+ * to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA
+ *
+ * Configurable idle (no activity) timer and logout redirect for jQuery.
+ * Works cross-browser with multiple windows and tabs within the same domain.
+ *
+ * Dependencies: JQuery v1.7+, JQuery UI, store.js from https://github.com/marcuswestin/store.js - v1.3.4+
+ *
+ * Commented and console logged for debugging with Firefox & Firebug or similar
+ * v1.0.3
+ */
 
 (function($) {
 
@@ -23,15 +21,16 @@
     //## Configuration Variables
     //##############################
     var defaults = {
-      //idleTimeLimit:      1200000,        // 'No activity' time limit in milliseconds. 1200000 = 20 Minutes
-      idleTimeLimit:        30000,          // 30 seconds for testing
-      //dialogDisplayLimit: 180000,         // Time to display the warning dialog before redirect (and optional callback) in milliseconds. 180000 = 3 Minutes
-      dialogDisplayLimit:   20000,          // 20 seconds for testing
-      redirectUrl:          '/logout',      // redirect to this url
+      //idleTimeLimit: 1200000,      // 'No activity' time limit in milliseconds. 1200000 = 20 Minutes
+      idleTimeLimit: 30000,          // 30 seconds for testing
+      //dialogDisplayLimit: 180000,  // Time to display the warning dialog before redirect (and optional callback) in milliseconds. 180000 = 3 Minutes
+      dialogDisplayLimit: 20000,     // 20 seconds for testing
+      redirectUrl: '/logout',        // redirect to this url
 
       // optional custom callback to perform before redirect
-      customCallback:       false,          // set to false for no customCallback
-      // customCallback:    function() {    // define optional custom js function
+      customCallback: false,         // set to false for no customCallback
+      // define optional custom js function
+      // customCallback: function() {
           // User to be logged out, perform custom action
       // },
 
@@ -39,17 +38,17 @@
       // http://www.quirksmode.org/dom/events/
       // https://developer.mozilla.org/en-US/docs/Web/Reference/Events
       // JQuery on() method (v1.7+) expects a 'space-separated' string of event names
-      // activityEvents:       'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
-      activityEvents:       'click keypress scroll wheel mousewheel', // customize events for testing - remove mousemove
+      // activityEvents: 'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
+      activityEvents: 'click keypress scroll wheel mousewheel', // customize events for testing - remove mousemove
 
       //dialog box configuration
-      dialogTitle:          'Session Expiration Warning',
-      dialogText:           'Because you have been inactive, your session is about to expire.',
+      dialogTitle: 'Session Expiration Warning',
+      dialogText: 'Because you have been inactive, your session is about to expire.',
 
       // server-side session keep-alive timer & url
       sessionKeepAliveTimer: 60000, // Ping the server at this interval in milliseconds. 60000 = 1 Minute
       // sessionKeepAliveTimer: false, // Set to false to disable pings.
-      sessionKeepAliveUrl:   '/', // url to ping
+      sessionKeepAliveUrl: '/', // url to ping
     };
 
     //##############################
@@ -64,20 +63,20 @@
     //##############################
 
     // open warning dialog function
-    var open_warning_dialog = function() {
+    var openWarningDialog = function() {
       var dialogContent = "<div id='idletimer_warning_dialog'><p>" + opts.dialogText + "</p></div>";
 
       var warningDialog = $(dialogContent).dialog({
         buttons: {
           "Stay Logged In": function() {
             console.log('Stay Logged In button clicked');
-            destroy_warning_dialog();
-            stop_dialog_timer();
-            start_idle_timer();
+            destroyWarningDialog();
+            stopDialogTimer();
+            startIdleTimer();
           },
           "Log Out Now": function() {
             console.log('Log Out Now button clicked');
-            logout_user();
+            logoutUser();
           }
         },
         closeOnEscape: false,
@@ -90,7 +89,7 @@
     };
 
     // is dialog open function
-    var is_dialog_open = function() {
+    var isDialogOpen = function() {
       var dialogOpen = $('#idletimer_warning_dialog').dialog('isOpen');
 
       if (dialogOpen === true) {
@@ -101,7 +100,7 @@
     };
 
     // destroy warning dialog function
-    var destroy_warning_dialog = function() {
+    var destroyWarningDialog = function() {
       console.log('dialog destroyed');
       $(".ui-dialog-content").dialog('destroy').remove();
     };
@@ -113,26 +112,26 @@
 
       if (timeNow > timeIdleTimeout) {
         console.log('timeNow: ' + timeNow + ' > idle ' + timeIdleTimeout);
-        if (is_dialog_open() !== true) {
+        if (isDialogOpen() !== true) {
           console.log('dialog is not open & will be opened');
-          open_warning_dialog();
-          start_dialog_timer();
+          openWarningDialog();
+          startDialogTimer();
         }
       } else if (store.get('idleTimerLoggedOut') === true) { //a 'manual' user logout?
-        logout_user();
+        logoutUser();
       } else {
         console.log('idle not yet timed out');
-        if (is_dialog_open() === true) {
+        if (isDialogOpen() === true) {
           console.log('dialog is open & will be closed');
-          destroy_warning_dialog();
-          stop_dialog_timer();
+          destroyWarningDialog();
+          stopDialogTimer();
         }
       }
     };
 
     // start idle timer function
-    var start_idle_timer = function() {
-      stop_idle_timer();
+    var startIdleTimer = function() {
+      stopIdleTimer();
       idleTimerLastActivity = $.now();
 
       store.set('idleTimerLastActivity', idleTimerLastActivity);
@@ -142,7 +141,7 @@
     };
 
     // stop idle timer function
-    var stop_idle_timer = function() {
+    var stopIdleTimer = function() {
       clearInterval(idleTimer);
     };
 
@@ -153,24 +152,24 @@
 
       if ((timeNow > timeDialogTimeout) || (store.get('idleTimerLoggedOut') === true)) {
         console.log('timeNow: ' + timeNow + ' > dialog' + timeDialogTimeout);
-        logout_user();
+        logoutUser();
       } else {
         console.log('dialog not yet timed out');
       }
     };
 
     // start dialog timer function
-    var start_dialog_timer = function() {
+    var startDialogTimer = function() {
       dialogTimer = setInterval(checkDialogTimeout, checkHeartbeat);
     };
 
     // stop dialog timer function
-    var stop_dialog_timer = function() {
+    var stopDialogTimer = function() {
       clearInterval(dialogTimer);
     };
 
     // perform logout procedure function
-    var logout_user = function() {
+    var logoutUser = function() {
       console.log('logout function');
       store.set('idleTimerLoggedOut', true);
 
@@ -186,13 +185,13 @@
 
     // activity detector function
     // if warning dialog is NOT open, restarts idle timer
-    var activity_detector = function() {
+    var activityDetector = function() {
 
       $('body').on(opts.activityEvents, function() {
 
-        if (is_dialog_open() !== true) {
+        if (isDialogOpen() !== true) {
           console.log('activity detected');
-          start_idle_timer();
+          startIdleTimer();
         } else {
           console.log('dialog open. activity ignored');
         }
@@ -202,18 +201,18 @@
 
     // if keep-alive sessionKeepAliveTimer value is not false,
     // ping the server at regular intervals to prevent a server idle timeout
-    var keep_session_alive = function() {
+    var keepSessionAlive = function() {
 
       if (opts.sessionKeepAliveTimer) {
-        var keep_session = function() {
+        var keepSession = function() {
           // if this is the most recently active window or tab
-          if (idleTimerLastActivity == store.get('idleTimerLastActivity')) {
+          if (idleTimerLastActivity === store.get('idleTimerLastActivity')) {
             console.log('keep session alive function');
             $.get(opts.sessionKeepAliveUrl);
           }
         };
 
-        setInterval(keep_session, opts.sessionKeepAliveTimer);
+        setInterval(keepSession, opts.sessionKeepAliveTimer);
       }
     };
 
@@ -233,11 +232,11 @@
         alert('Dependent file missing. Please see: https://github.com/marcuswestin/store.js');
       }
 
-      activity_detector();
+      activityDetector();
 
-      keep_session_alive();
+      keepSessionAlive();
 
-      start_idle_timer();
+      startIdleTimer();
     });
   }
 })(jQuery);
--- jquery-idleTimeout.js
+++ jquery-idleTimeout.js
@@ -1,17 +1,15 @@
-//######
-//## This work is licensed under the Creative Commons Attribution-Share Alike 3.0
-//## United States License. To view a copy of this license,
-//## visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter
-//## to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
-//######
-
-// Dependency: https://github.com/marcuswestin/store.js - version 1.3.4+
-
-// Thanks to https://github.com/philpalmieri/jquery-idleTimeout & https://github.com/josebalius/jquery-idleTimeout
-// Modified By: Jill Elaine
-// Email: jillelaine01(at)gmail(dot)com
-
-// version 1.0.2
+/**
+ * This work is licensed under the Creative Commons Attribution-Share Alike 3.0
+ * United States License. To view a copy of this license,
+ * visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter
+ * to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA
+ *
+ * Configurable idle (no activity) timer and logout redirect for jQuery.
+ * Works cross-browser with multiple windows and tabs within the same domain.
+ *
+ * Dependencies: JQuery v1.7+, JQuery UI, store.js from https://github.com/marcuswestin/store.js - v1.3.4+
+ * v1.0.3
+ */
 
 (function($) {
 
@@ -20,12 +18,12 @@
     //## Configuration Variables
     //##############################
     var defaults = {
-      idleTimeLimit:      1200000,        // 'No activity' time limit in milliseconds. 1200000 = 20 Minutes
-      dialogDisplayLimit: 180000,         // Time to display the warning dialog before redirect (and optional callback) in milliseconds. 180000 = 3 Minutes
-      redirectUrl:        '/logout',      // redirect to this url
+      idleTimeLimit: 1200000,       // 'No activity' time limit in milliseconds. 1200000 = 20 Minutes
+      dialogDisplayLimit: 180000,   // Time to display the warning dialog before redirect (and optional callback) in milliseconds. 180000 = 3 Minutes
+      redirectUrl: '/logout',       // redirect to this url
 
       // optional custom callback to perform before redirect
-      customCallback:       false,          // set to false for no customCallback
+      customCallback: false,       // set to false for no customCallback
       // customCallback:    function() {    // define optional custom js function
           // perform custom action before logout
       // },
@@ -33,16 +31,16 @@
       // configure which activity events to detect
       // http://www.quirksmode.org/dom/events/
       // https://developer.mozilla.org/en-US/docs/Web/Reference/Events
-      activityEvents:       'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
+      activityEvents: 'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
 
       //dialog box configuration
-      dialogTitle:          'Session Expiration Warning',
-      dialogText:           'Because you have been inactive, your session is about to expire.',
+      dialogTitle: 'Session Expiration Warning',
+      dialogText: 'Because you have been inactive, your session is about to expire.',
 
       // server-side session keep-alive timer & url
       sessionKeepAliveTimer: 60000, // Ping the server at this interval in milliseconds. 60000 = 1 Minute
       // sessionKeepAliveTimer: false, // Set to false to disable pings.
-      sessionKeepAliveUrl:   '/', // url to ping
+      sessionKeepAliveUrl: '/',  // url to ping
     };
 
     //##############################
@@ -56,18 +54,18 @@
     //## Private Functions
     //##############################
 
-    var open_warning_dialog = function() {
+    var openWarningDialog = function() {
       var dialogContent = "<div id='idletimer_warning_dialog'><p>" + opts.dialogText + "</p></div>";
 
       var warningDialog = $(dialogContent).dialog({
         buttons: {
           "Stay Logged In": function() {
-            destroy_warning_dialog();
-            stop_dialog_timer();
-            start_idle_timer();
+            destroyWarningDialog();
+            stopDialogTimer();
+            startIdleTimer();
           },
           "Log Out Now": function() {
-            logout_user();
+            logoutUser();
           }
         },
         closeOnEscape: false,
@@ -79,7 +77,7 @@
       $('.ui-dialog-titlebar-close').css('display', 'none');
     };
 
-    var is_dialog_open = function() {
+    var isDialogOpen = function() {
       var dialogOpen = $('#idletimer_warning_dialog').dialog('isOpen');
 
       if (dialogOpen === true) {
@@ -89,7 +87,7 @@
       }
     };
 
-    var destroy_warning_dialog = function() {
+    var destroyWarningDialog = function() {
       $(".ui-dialog-content").dialog('destroy').remove();
     };
 
@@ -98,28 +96,28 @@
       var timeIdleTimeout = (store.get('idleTimerLastActivity') + opts.idleTimeLimit);
 
       if (timeNow > timeIdleTimeout) {
-        if (is_dialog_open() !== true) {
-          open_warning_dialog();
-          start_dialog_timer();
+        if (isDialogOpen() !== true) {
+          openWarningDialog();
+          startDialogTimer();
         }
       } else if (store.get('idleTimerLoggedOut') === true) { //a 'manual' user logout?
-        logout_user();
+        logoutUser();
       } else {
-        if (is_dialog_open() === true) {
-          destroy_warning_dialog();
-          stop_dialog_timer();
+        if (isDialogOpen() === true) {
+          destroyWarningDialog();
+          stopDialogTimer();
         }
       }
     };
 
-    var start_idle_timer = function() {
-      stop_idle_timer();
+    var startIdleTimer = function() {
+      stopIdleTimer();
       idleTimerLastActivity = $.now();
       store.set('idleTimerLastActivity', idleTimerLastActivity);
       idleTimer = setInterval(checkIdleTimeout, checkHeartbeat);
     };
 
-    var stop_idle_timer = function() {
+    var stopIdleTimer = function() {
       clearInterval(idleTimer);
     };
 
@@ -128,19 +126,19 @@
       var timeDialogTimeout = (store.get('idleTimerLastActivity') + opts.idleTimeLimit + opts.dialogDisplayLimit);
 
       if ((timeNow > timeDialogTimeout) || (store.get('idleTimerLoggedOut') === true)) {
-        logout_user();
+        logoutUser();
       }
     };
 
-    var start_dialog_timer = function() {
+    var startDialogTimer = function() {
       dialogTimer = setInterval(checkDialogTimeout, checkHeartbeat);
     };
 
-    var stop_dialog_timer = function() {
+    var stopDialogTimer = function() {
       clearInterval(dialogTimer);
     };
 
-    var logout_user = function() {
+    var logoutUser = function() {
       store.set('idleTimerLoggedOut', true);
 
       if (opts.customCallback) {
@@ -152,26 +150,26 @@
       }
     };
 
-    var activity_detector = function() {
+    var activityDetector = function() {
 
       $('body').on(opts.activityEvents, function() {
 
-        if (is_dialog_open() !== true) {
-          start_idle_timer();
+        if (isDialogOpen() !== true) {
+          startIdleTimer();
         }
       });
     };
 
-    var keep_session_alive = function() {
+    var keepSessionAlive = function() {
 
       if (opts.sessionKeepAliveTimer) {
-        var keep_session = function() {
-          if (idleTimerLastActivity == store.get('idleTimerLastActivity')) {
+        var keepSession = function() {
+          if (idleTimerLastActivity === store.get('idleTimerLastActivity')) {
             $.get(opts.sessionKeepAliveUrl);
           }
         };
 
-        setInterval(keep_session, opts.sessionKeepAliveTimer);
+        setInterval(keepSession, opts.sessionKeepAliveTimer);
       }
     };
 
@@ -189,11 +187,11 @@
         alert('Dependent file missing. Please see: https://github.com/marcuswestin/store.js');
       }
 
-      activity_detector();
+      activityDetector();
 
-      keep_session_alive();
+      keepSessionAlive();
 
-      start_idle_timer();
+      startIdleTimer();
     });
   }
 })(jQuery);
